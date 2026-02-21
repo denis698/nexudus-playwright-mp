c7fea7c02d84840b8378bd4e7fb218468d478f6c
denis <denis@nexudus.com>
2026-02-21 19:10:37 +0000

    removing redundant code

--- e2e/test/mp.navigation.test.ts
+++ e2e/test/mp.navigation.test.ts
@@ -2,7 +2,7 @@ import test from '@lib/BaseTest';
 import menuData from './testdata/mp/navigation/profile_menu.json';
 import { expect } from '@playwright/test';
 
-test.beforeEach(async ({ mpLoginPage, mpCommonPage }) => {
+test.beforeEach(async ({ mpLoginPage }) => {
   await mpLoginPage.navigateTo(process.env.MP_TEST_LOGIN_PAGE_URL);
   await mpLoginPage.login(String(process.env.MP_LOCATION_PASSWORD));
   await mpLoginPage.verifyAt();
